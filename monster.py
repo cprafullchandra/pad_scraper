fbbe467d5d71e047406c7b63cded7222381bec8c
Robert Wang <robert.wang@workday.com>
2017-02-09 00:00:25 -0800

    Added monster class and included monster skills and awakenings. Evolutions next

--- monster.py
+++ monster.py
@@ -4,26 +4,62 @@ class Monster:
       self.id = 0
       self.avatar = ""
       self.image = ""
-      self.name = ""
+      self.en_name = ""
+      self.jp_name = ""
       self.type = ""
       self.element = []
       self.rarity = 0
       self.cost = 0
-      self.monsterpoints = 0
-      self.maxlvl = 0
-      self.baseatk = 0
-      self.basehp = 0
-      self.basercv = 0
-      self.maxatk = 0
-      self.maxhp = 0
-      self.maxrcv = 0
+      self.monster_points = 0
+      self.min_level = 0
+      self.max_level = 0
+      self.base_atk = 0
+      self.base_hp = 0
+      self.base_rcv = 0
+      self.max_atk = 0
+      self.max_hp = 0
+      self.max_rcv = 0
+      self.base_weighted_stats = 0
+      self.max_weighted_stats = 0
+      self.max_exp = 0
+      self.exp_curve = 0
+      self.active_skill = ""
+      self.active_skill_description = ""
+      self.active_skill_cooldown = ""
+      self.same_active_skill = []
+      self.leader_skill = ""
+      self.leader_skill_description = ""
+      self.awakenings = []
       self.evolutions = []
 
   def info(self):
       print("ID: " + str(self.id))
-      print("Name: " + self.name)
+      print("EN Name: " + self.en_name)
+      print("JP Name: " + self.jp_name)
       print("Avatar: " + self.avatar)
       print("Image: " + self.image)
       print("Type: " + str(self.type))
+      print("Element: " + str(self.element))
       print("Rarity: " + str(self.rarity))
+      print("Cost: " + str(self.cost))
+      print("Monster Points: " + str(self.monster_points))
+      print("Min Level: " + str(self.min_level))
+      print("Base HP: " + str(self.base_hp))
+      print("Base ATK: " + str(self.base_atk))
+      print("Base RCV: " + str(self.base_rcv))
+      print("Max Level: " + str(self.max_level))
+      print("Max HP: " + str(self.max_hp))
+      print("Max ATK: " + str(self.max_atk))
+      print("Max RCV: " + str(self.max_rcv))
+      print("Base Weighted Stats: " + str(self.base_weighted_stats))
+      print("Max Weighted Stats: " + str(self.max_weighted_stats))
+      print("Growth Curve: " + str(self.exp_curve))
+      print("Max EXP: " + str(self.max_exp))
+      print("Active Skill: " + self.active_skill)
+      print("Active Skill Description: " + self.active_skill_description)
+      print("Active Skill Cooldown: " + self.active_skill_cooldown)
+      print("Same Active Skill: " + str(self.same_active_skill))
+      print("Leader Skill: " + self.leader_skill)
+      print("Leader Skill Description: " + self.leader_skill_description)
+      print("Awakenings: " + str(self.awakenings))
       print("Evolutions: " + str(self.evolutions))
--- scraper.py
+++ scraper.py
@@ -27,13 +27,43 @@ for mon_id in range(2972,2973):
 	tree = html.fromstring(page.content)
 
 	monster = Monster()
-	# get monster image url
+
+	### Get monster information from tables
 	monster.id = mon_id
 	monster.image = pad_url + tree.xpath('//div[@id="monster"]/a')[0].attrib['href']
 	monster.avatar = pad_url + tree.xpath('//div[@id="content"]/div[@class="avatar"]/img')[0].attrib['src']
-	monster.name = tree.xpath('//div[@id="content"]/div[@class="name"]//text()')[0]
-	monster.rarity = int(tree.xpath('count(//div[@id="content"]/div[@class="stars"]/img)'))
-	monster.type = tree.xpath('//div[@id="content"]//td[@class="ptitle" and text()="Type:"]/../td[@class="data"]/a/text()')
 
-	# Only care about tables 1, 3, and 4
+	### Table 1 info
+	monster.en_name = tree.xpath('//div[@id="content"]/div[@class="name"]//text()')[0]
+	monster.jp_name = tree.xpath('//div[@id="content"]//td[@class="data jap"]/text()')[0]
+	monster.type = tree.xpath('//div[@id="content"]//td[@class="ptitle" and text()="Type:"]/../td[@class="data"]/a/text()')
+	monster.element = tree.xpath('//div[@id="content"]//td[@class="ptitle" and text()="Element:"]/../td[@class="data"]/a/text()')
+	monster.rarity = int(tree.xpath('count(//div[@id="content"]/div[@class="stars"]/img)'))
+	monster.cost = int(tree.xpath('//div[@id="content"]//td[@class="ptitle" and text()="Cost:"]/../td[@class="data"]/a/text()')[0])
+	monster.monsterpoints = int(tree.xpath('//div[@id="content"]//span[@title="Monster Point"]/../../td[@class="data"]/text()')[0])
+	monster.expcurve = int(tree.xpath('//div[@id="compareprofile"]/table[@id="tablestat"]//td[contains(.,"Growth Curve")]/a/text()')[0].replace(',',''))
+	monster.maxexp = int(re.search('[0-9]+',tree.xpath('//div[@id="compareprofile"]/table[@id="tablestat"]//td[contains(.,"Exp to max")]/text()')[0].replace(',','')).group(0))
+
+	### Table 3 info
+	monster.minlvl = int(tree.xpath('//div[@id="comparechart"]//td[text()="Level"]/following-sibling::td/text()')[0])
+	monster.maxlvl = int(tree.xpath('//div[@id="comparechart"]//td[text()="Level"]/following-sibling::td/following-sibling::td/text()')[0])
+	monster.basehp = int(tree.xpath('//div[@id="comparechart"]//td[text()="HP"]/following-sibling::td/text()')[0])
+	monster.maxhp = int(tree.xpath('//div[@id="comparechart"]//td[text()="HP"]/following-sibling::td/following-sibling::td/text()')[0])
+	monster.baseatk = int(tree.xpath('//div[@id="comparechart"]//td[text()="ATK"]/following-sibling::td/text()')[0])
+	monster.maxatk = int(tree.xpath('//div[@id="comparechart"]//td[text()="ATK"]/following-sibling::td/following-sibling::td/text()')[0])
+	monster.basercv = int(tree.xpath('//div[@id="comparechart"]//td[text()="RCV"]/following-sibling::td/text()')[0])
+	monster.maxrcv = int(tree.xpath('//div[@id="comparechart"]//td[text()="RCV"]/following-sibling::td/following-sibling::td/text()')[0])
+	### Weighted stats are HP/10 + ATK/5 + RCV/3
+	monster.minweighted = monster.basehp/10 + monster.baseatk/5 + monster.basercv/3
+	monster.maxweighted = monster.maxhp/10 + monster.maxatk/5 + monster.maxrcv/3
+
+	### Table 4 info
+	monster.active_skill = tree.xpath('//div[@id="content"]//td[@class="title value-normal nowrap" and text()="Active Skill:"]/following-sibling::td/a/span/text()')[0]
+	monster.active_skill_description = tree.xpath('//div[@id="content"]//td[@class="title" and text()="Effects:"]/following-sibling::td/text()')[0]
+	monster.active_skill_cooldown = tree.xpath('//div[@id="content"]//td[@class="title" and text()="Cool Down:"]/following-sibling::td/text()')[0]
+	monster.same_active_skill = [re.search('[0-9]+', x.attrib['href']).group(0) for x in tree.xpath('//div[@id="content"]//td[@class="title" and text()="Same Skill:"]/following-sibling::td/a')]
+	monster.leader_skill = tree.xpath('//div[@id="content"]//td[@class="title value-normal nowrap" and text()="Leader Skill:"]/following-sibling::td/a/span/text()')[0]
+	monster.leader_skill_description = tree.xpath('//div[@id="content"]//td[@class="title" and text()="Effects:"]/following-sibling::td/text()')[1]
+	monster.awakenings = [re.search('^(.*?)(?=\r\n)', x.attrib['title']).group(0) for x in tree.xpath('//div[@id="content"]//td[@class="awoken1"]/a/img')]
+
 	monster.info()
